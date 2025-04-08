0bc1e3bbd2de44c63a93a55d4381d152a23800ff
Furti <furtiadi@gmail.com>
2025-04-08 13:56:06 +0200

    Fixed filtering

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,29 +1,40 @@
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn . model_selection import train_test_split
+
 
 def preprocess_data():
+    
     # Load ratings
     ratings = pd.read_csv('datasets/ratings.csv')
 
     # Step 1: Remove exact duplicate rows
     duplicate_rows = ratings.duplicated()
-    #print(f"Total dublicate rows: {duplicate_rows}")
+    print(f"Total dublicate rows: {duplicate_rows.sum()}")
     if duplicate_rows.any():
         ratings = ratings.drop_duplicates()
 
     # Step 2: Handle duplicate user-movie ratings by averaging them
     duplicate_user_movie = ratings.duplicated(subset=['UserID', 'MovieID'])
-    #print(f"Total dublicate user movies: {duplicate_user_movie}")
+    print(f"Total dublicate user movies: {duplicate_user_movie.sum()}")
     if duplicate_user_movie.any():
         ratings = ratings.groupby(['UserID', 'MovieID'], as_index=False)['Rating'].mean()
 
-    # Step 3: Filter out movies with fewer than 20 ratings
-    movie_counts = ratings['MovieID'].value_counts()
-    valid_movies = movie_counts[movie_counts >= 20].index
-    ratings = ratings[ratings['MovieID'].isin(valid_movies)]
+    # Step 3: Filter active users and popular movies
+    user_counts = ratings['UserID'].value_counts()
+    selected_users = user_counts[user_counts >= 100].sample(n=500, random_state=42).index
+    ratings = ratings[ratings['UserID'].isin(selected_users)]
 
-    # Step 4: Create User-item Rating Matrix
+    movie_counts = ratings['MovieID'].value_counts()
+    selected_movies = movie_counts[movie_counts >= 50].sample(n=300, random_state=42).index
+    ratings = ratings[ratings['MovieID'].isin(selected_movies)]
+
+    num_users = ratings['UserID'].nunique()
+    num_movies = ratings['MovieID'].nunique()
+    print(f"Filtered dataset contains {num_users} users and {num_movies} movies.")
+
+    # Step 4: Create User-item Matrix
     user_item_matrix = ratings.pivot_table(index='UserID', columns='MovieID', values='Rating')
 
     # Step 5: Check for missing-values and if any, fill missing values with user mean
@@ -34,16 +45,17 @@ def preprocess_data():
         user_item_filled = user_item_matrix
     
     # Confirm all missing values are filled
-    #any_missing_values_left = user_item_filled.isnull().sum().sum()
-    #print(f"Total missing values after filling: {any_missing_values_left}")
+    any_missing_values_left = user_item_filled.isnull().sum().sum()
+    print(f"Total missing values after filling: {any_missing_values_left}")
 
-    # Step 6: Apply PCA
-    pca = PCA(n_components=0.95)
-    reduced_user_item_matrix = pca.fit_transform(user_item_filled)
+    # Step 6: Split dataset into train and test test_size 80/20)
+    train_set, test_set = train_test_split(user_item_filled, test_size=0.2, random_state=0)
+    
+  
+    train_set.to_csv("datasets/processed_train_dataset.csv")
+    test_set.to_csv("datasets/processed_test_dataset.csv")
+    user_item_filled.to_csv("datasets/processed_user_item_matrix.csv")
 
-    #print(f"Original shape: {user_item_normalized.shape}")
-    #print(f"Reduced shape: {user_item_reduced.shape}")
-    #print(f"Explained variance: {np.sum(pca.explained_variance_ratio_):.2%}")
-
-    # Returns processed matrix, rating and orginal matrix
-    return reduced_user_item_matrix, ratings, user_item_matrix
+    
+if __name__ == "__main__":
+    preprocess_data()
